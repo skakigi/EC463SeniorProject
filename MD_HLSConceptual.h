5afc8ca1a6f9071613e1f53d0de3513f56819b75
skakigi <skakigi@bu.edu>
2024-09-18 21:22:24 -0400

    Neighbor list version creation / changed coordinates of particles to float and setup constructor

--- MD_HLSConceptual.h
+++ MD_HLSConceptual.h
@@ -9,16 +9,19 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 //parameters
 
 #define NUM_PARTICLES_PER_CELL 200
-
+#define NUM_CELLS 100
+#define TOTAL_GRID_SIZE_DIMENSION 1000
 
 //starting with particle
 typedef struct{
     //coordinate location
-    int x,y,z;
+    float x,y,z;
 
     //cell number
     int cell;
@@ -33,9 +36,23 @@ typedef struct{
     bool isFull;
 }Cell;
 
+//Helper Functions
+
 int find_neighbors(const Cell *cellList);
 int find_distance(const Particle A,const Particle B);
-int setup_particles();
+
+//Constructors
+Particle setup_particles(){
+    srand((unsigned int)time(NULL));
+
+    Particle particle;
+    particle.x = ((float)rand()/(float)(RAND_MAX)) * TOTAL_GRID_SIZE_DIMENSION;
+    particle.y = ((float)rand()/(float)(RAND_MAX)) * TOTAL_GRID_SIZE_DIMENSION;
+    particle.z = ((float)rand()/(float)(RAND_MAX)) * TOTAL_GRID_SIZE_DIMENSION;
+
+    particle.cell = -1;
+
+}
 
 
 
---
+++ MD_HLSNeighborList.c
@@ -0,0 +1,18 @@
+//
+// Created by sorak on 9/18/2024.
+//
+
+//.h file
+#include "MD_HLSConceptual.h"
+
+//included libraries
+#include <stdlib.h>
+
+int setupParticles(){
+    Particle particleList[NUM_CELLS * NUM_PARTICLES_PER_CELL];
+}
+
+
+int main(){
+
+}
