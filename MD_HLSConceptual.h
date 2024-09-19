deaeb66ed3e865c85cb5f58c163993eb6c72178a
skakigi <skakigi@bu.edu>
2024-09-18 23:26:35 -0400

    refining structs and defining init func | still need to organize particles into cells

--- MD_HLSConceptual.h
+++ MD_HLSConceptual.h
@@ -11,12 +11,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <math.h>
 
 //parameters
 
 #define NUM_PARTICLES_PER_CELL 200
 #define NUM_CELLS 100
-#define TOTAL_GRID_SIZE_DIMENSION 1000
+#define CELL_SIZE_DIMENSION 10
+#define GRID_SIZE_DIMENSION 1000
+#define CELL_COUNT_ACROSS GRID_SIZE_DIMENSION/CELL_SIZE_DIMENSION
+#define TOTAL_PARTICLE_COUNT NUM_PARTICLES_PER_CELL * NUM_CELLS
+#define TOTAL_CELL_COUNT (int)(pow(GRID_SIZE_DIMENSION,3)/pow(CELL_SIZE_DIMENSION,3))
 
 //starting with particle
 typedef struct{
@@ -32,6 +37,9 @@ typedef struct{
     //assume each cell has NUM_PARTICLES_PER_CELL particles
     Particle Particles[NUM_PARTICLES_PER_CELL];
 
+    //cell location (has to be integer x,y,z)
+    int x,y,z;
+
     //quick check if the cell is full (mostly for setup)
     bool isFull;
 }Cell;
@@ -40,18 +48,30 @@ typedef struct{
 
 int find_neighbors(const Cell *cellList);
 int find_distance(const Particle A,const Particle B);
+void set_cell_coordinate(Cell *cell,int x,int y,int z){
+    cell->x=x;
+    cell->y=y;
+    cell->z=z;
+}
+void set_particle_coordinate(Particle *particle,float x, float y, float z){
+    particle->x=x;
+    particle->y=y;
+    particle->z=z;
+}
+void init(Particle *particleList,Cell cellList[CELL_COUNT_ACROSS][CELL_COUNT_ACROSS][CELL_COUNT_ACROSS]);
 
 //Constructors
-Particle setup_particles(){
+Particle setup_particle(){
     srand((unsigned int)time(NULL));
 
     Particle particle;
-    particle.x = ((float)rand()/(float)(RAND_MAX)) * TOTAL_GRID_SIZE_DIMENSION;
-    particle.y = ((float)rand()/(float)(RAND_MAX)) * TOTAL_GRID_SIZE_DIMENSION;
-    particle.z = ((float)rand()/(float)(RAND_MAX)) * TOTAL_GRID_SIZE_DIMENSION;
+    particle.x = ((float)rand()/(float)(RAND_MAX)) * GRID_SIZE_DIMENSION;
+    particle.y = ((float)rand()/(float)(RAND_MAX)) * GRID_SIZE_DIMENSION;
+    particle.z = ((float)rand()/(float)(RAND_MAX)) * GRID_SIZE_DIMENSION;
 
     particle.cell = -1;
 
+    return particle;
 }
 
 
--- MD_HLSNeighborList.c
+++ MD_HLSNeighborList.c
@@ -5,14 +5,51 @@
 //.h file
 #include "MD_HLSConceptual.h"
 
-//included libraries
-#include <stdlib.h>
-
-int setupParticles(){
-    Particle particleList[NUM_CELLS * NUM_PARTICLES_PER_CELL];
+/*--------------------- functions start below ---------------------*/
+//error handler
+void handleError(const char *message){
+    fprintf(stderr, "Error: %s\n",message);
+    exit(EXIT_FAILURE);
 }
 
+void init(Particle *particleList, Cell cellList[CELL_COUNT_ACROSS][CELL_COUNT_ACROSS][CELL_COUNT_ACROSS]){
+
+    //checking if particle and cell lists exist
+    if(particleList == NULL){
+        handleError("particleList is NULL : failed to init\n");
+    }
+    if(cellList == NULL){
+        handleError("cellList is NULL : failed to init\n");
+    }
+    //setup all the particles
+    for(int i=0; i<TOTAL_PARTICLE_COUNT; i++){
+
+        //setup particle in MD_HLSConceptual.h
+        particleList[i] = setup_particle();
+    }
+
+    //setup all the cells
+    for(int z=0; z<CELL_COUNT_ACROSS;z++){
+        for(int y=0; y<CELL_COUNT_ACROSS;y++){
+            for(int x=0; x<CELL_COUNT_ACROSS;x++){
+                //for each cell (x,y,z) setup cell
+                set_cell_coordinate(&(cellList[x][y][z]),x,y,z);
+                cellList[x][y][z].isFull=false;
+            }
+        }
+    }
+
+    //organize particles into cells
+
+
+
+}
 
 int main(){
+    Particle particleList[TOTAL_PARTICLE_COUNT];
+    Cell cellList[CELL_COUNT_ACROSS][CELL_COUNT_ACROSS][CELL_COUNT_ACROSS];
+    init(particleList,cellList);
+
+
 
 }
