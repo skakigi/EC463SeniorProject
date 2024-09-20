6f493d3b9714baca1ab684717944679aef882354
skakigi <skakigi@bu.edu>
2024-09-20 01:05:13 -0400

    try wokring with an ordered partcle generation over random

--- MD_HLSConceptual.h
+++ MD_HLSConceptual.h
@@ -15,6 +15,7 @@
 
 //parameters
 
+//basic building block parameters
 #define NUM_PARTICLES_PER_CELL 200
 #define NUM_CELLS 100
 #define CELL_SIZE_DIMENSION 10
@@ -22,6 +23,9 @@
 #define CELL_COUNT_ACROSS GRID_SIZE_DIMENSION/CELL_SIZE_DIMENSION
 #define TOTAL_PARTICLE_COUNT NUM_PARTICLES_PER_CELL * NUM_CELLS
 #define TOTAL_CELL_COUNT (int)(pow(GRID_SIZE_DIMENSION,3)/pow(CELL_SIZE_DIMENSION,3))
+
+//properties of particles
+#define EXCLUSION_RADIUS 1
 
 //starting with particle
 typedef struct{
@@ -39,6 +43,8 @@ typedef struct{
 
     //cell location (has to be integer x,y,z)
     int x,y,z;
+
+    int nextIndex;
 
     //quick check if the cell is full (mostly for setup)
     bool isFull;
@@ -58,10 +64,18 @@ void set_particle_coordinate(Particle *particle,float x, float y, float z){
     particle->y=y;
     particle->z=z;
 }
+int convert_cellX_to_coordinate(Cell *cell){
+    return cell->x*CELL_SIZE_DIMENSION;
+}
 void init(Particle *particleList,Cell cellList[CELL_COUNT_ACROSS][CELL_COUNT_ACROSS][CELL_COUNT_ACROSS]);
-
+void initCellList(Cell cellList[CELL_COUNT_ACROSS][CELL_COUNT_ACROSS][CELL_COUNT_ACROSS]);
 //Constructors
-Particle setup_particle(){
+
+/* ---------------------------------  Random Creation  --------------------------------- */
+/* This is a randomly generated particle method but will fail to use exclusion principle */
+Particle setup_particle_random(){
+
+
     srand((unsigned int)time(NULL));
 
     Particle particle;
@@ -71,7 +85,63 @@ Particle setup_particle(){
 
     particle.cell = -1;
 
+
     return particle;
+}
+
+/* ---------------------------------  Ordered Creation  --------------------------------- */
+/* This is an ordered creation that will start with exclusion principle in mind | will run the same if parameters are not changed */
+
+void setup_particle_ordered(int cellNum,Cell cellList[CELL_COUNT_ACROSS][CELL_COUNT_ACROSS][CELL_COUNT_ACROSS],Cell *currCell){
+    /* we need to initialize all particles in currCell */
+    float currX = 0;
+    float currY = 0;
+    float currZ = 0;
+    for(int i = 0; i<NUM_PARTICLES_PER_CELL; i++){
+        Particle currParticle;
+        if(currX!=CELL_SIZE_DIMENSION){
+
+            float xConvert = currX + currCell->x*CELL_SIZE_DIMENSION;
+            float yConvert = currY + currCell->y*CELL_SIZE_DIMENSION;
+            float zConvert = currZ + currCell->z*CELL_SIZE_DIMENSION;
+
+            set_particle_coordinate(&currParticle,xConvert,yConvert,zConvert);
+            currCell->Particles[currCell->nextIndex] = currParticle;
+            currCell->nextIndex++;
+
+            currX+=EXCLUSION_RADIUS;
+        }
+        else if(currY!=CELL_SIZE_DIMENSION){
+            currX=0;
+            currY+=EXCLUSION_RADIUS;
+
+            float xConvert = currX + currCell->x*CELL_SIZE_DIMENSION;
+            float yConvert = currY + currCell->y*CELL_SIZE_DIMENSION;
+            float zConvert = currZ + currCell->z*CELL_SIZE_DIMENSION;
+
+            set_particle_coordinate(&currParticle,xConvert,yConvert,zConvert);
+            currCell->Particles[currCell->nextIndex] = currParticle;
+            currCell->nextIndex++;
+
+            currX+=EXCLUSION_RADIUS;
+        }
+        else{
+            currX = 0;
+            currY = 0;
+            currZ +=EXCLUSION_RADIUS;
+
+            float xConvert = currX + currCell->x*CELL_SIZE_DIMENSION;
+            float yConvert = currY + currCell->y*CELL_SIZE_DIMENSION;
+            float zConvert = currZ + currCell->z*CELL_SIZE_DIMENSION;
+
+
+            set_particle_coordinate(&currParticle,xConvert,yConvert,zConvert);
+            currCell->Particles[currCell->nextIndex] = currParticle;
+            currCell->nextIndex++;
+
+            currX+=EXCLUSION_RADIUS;
+        }
+    }
 }
 
 
--- MD_HLSNeighborList.c
+++ MD_HLSNeighborList.c
@@ -4,6 +4,7 @@
 
 //.h file
 #include "MD_HLSConceptual.h"
+#define RANDOM_PARTICLE_GEN false
 
 /*--------------------- functions start below ---------------------*/
 //error handler
@@ -11,7 +12,19 @@ void handleError(const char *message){
     fprintf(stderr, "Error: %s\n",message);
     exit(EXIT_FAILURE);
 }
-
+void initCellList(Cell cellList[CELL_COUNT_ACROSS][CELL_COUNT_ACROSS][CELL_COUNT_ACROSS]){
+    /* Setup Cell Blocks in Grid */
+    for (int z = 0; z < CELL_COUNT_ACROSS; z++) {
+        for (int y = 0; y < CELL_COUNT_ACROSS; y++) {
+            for (int x = 0; x < CELL_COUNT_ACROSS; x++) {
+                //for each cell (x,y,z) setup cell
+                set_cell_coordinate(&(cellList[x][y][z]), x, y, z);
+                cellList[x][y][z].isFull = false;
+                cellList[x][y][z].nextIndex = 0;
+            }
+        }
+    }
+}
 void init(Particle *particleList, Cell cellList[CELL_COUNT_ACROSS][CELL_COUNT_ACROSS][CELL_COUNT_ACROSS]){
 
     //checking if particle and cell lists exist
@@ -21,25 +34,44 @@ void init(Particle *particleList, Cell cellList[CELL_COUNT_ACROSS][CELL_COUNT_AC
     if(cellList == NULL){
         handleError("cellList is NULL : failed to init\n");
     }
-    //setup all the particles
-    for(int i=0; i<TOTAL_PARTICLE_COUNT; i++){
 
-        //setup particle in MD_HLSConceptual.h
-        particleList[i] = setup_particle();
+
+    /* initialize cell list */
+    initCellList(cellList);
+
+    /* This section uses random particles */
+    if(RANDOM_PARTICLE_GEN) {
+        /* Generate all particles randomly */
+        for (int i = 0; i < TOTAL_PARTICLE_COUNT; i++) {
+            particleList[i] = setup_particle_random();
+            float currPartX = particleList->x;
+            float currPartY = particleList->y;
+            float currPartZ = particleList->z;
+
+            int cellX = (int)(currPartX/CELL_SIZE_DIMENSION);
+            int cellY = (int)(currPartY/CELL_SIZE_DIMENSION);
+            int cellZ = (int)(currPartZ/CELL_SIZE_DIMENSION);
+
+            int nextIndex = cellList[cellX][cellY][cellZ].nextIndex;
+
+            //there are probably out of bounds issues with this atm
+            if(nextIndex!=199) {
+                cellList[cellX][cellY][cellZ].Particles[nextIndex] = particleList[i];
+                cellList[cellX][cellY][cellZ].nextIndex++;
+            }
+        }
     }
-
-    //setup all the cells
-    for(int z=0; z<CELL_COUNT_ACROSS;z++){
-        for(int y=0; y<CELL_COUNT_ACROSS;y++){
-            for(int x=0; x<CELL_COUNT_ACROSS;x++){
-                //for each cell (x,y,z) setup cell
-                set_cell_coordinate(&(cellList[x][y][z]),x,y,z);
-                cellList[x][y][z].isFull=false;
+    /* this section uses ordered particle start */
+    else{
+        for (int z = 0; z < CELL_COUNT_ACROSS; z++) {
+            for (int y = 0; y < CELL_COUNT_ACROSS; y++) {
+                for (int x = 0; x < CELL_COUNT_ACROSS; x++) {
+                    setup_particle_ordered()
+                }
             }
         }
     }
 
-    //organize particles into cells
 
 
 
@@ -49,6 +81,9 @@ int main(){
     Particle particleList[TOTAL_PARTICLE_COUNT];
     Cell cellList[CELL_COUNT_ACROSS][CELL_COUNT_ACROSS][CELL_COUNT_ACROSS];
     init(particleList,cellList);
+
+    printf("The first particle has an x,y,z of %f,%f,%f",particleList[0].x,particleList[0].y,particleList[0].z);
+
 
 
 
